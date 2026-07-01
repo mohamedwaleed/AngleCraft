db4dcd2f46195d3cc0cad33c8289ed7e6b7cc069
Mohamed Waleed Mohamed <mohamedwaleed2012@gmail.com>
2026-07-01 15:05:07 +0200

    Update Facebook Pixel ID in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2124408331753576');
+fbq('init', '1752860786157575');
 fbq('track', 'PageView');`,
           }}
         />
