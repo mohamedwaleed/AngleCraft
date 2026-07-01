b05a0e378c8923fa19bf8f1e10d8b1d19984b57b
Mohamed Waleed <mohamedwaleed2012@gmail.com>
2026-07-01 23:03:14 +0200

    install microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,16 @@ fbq('track', 'PageView');`,
             alt=""
           />
         </noscript>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xftn7szvmr");`,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
